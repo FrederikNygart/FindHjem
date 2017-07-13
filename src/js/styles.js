397fad68fb2fd69656b9de71be310c7f2a3fa7fd
frederik nygart <frederik nygart>
2017-07-13 12:01:08 +0200

    added style file to handle styles

---
+++ src/js/styles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export const appStyle = StyleSheet.create({
+  resultText: {
+    backgroundColor: '#000',
+    color: '#FFF',
+    flex: 1,
+  },
+  resultImage: {
+    height: 150,
+  },
+  flashyBackground: {
+    flex:2,
+    backgroundColor: '#FC46AA'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#27408b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
