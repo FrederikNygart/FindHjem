4becc23b4e9ffab73a42cc805c94f6b5b2160196
frederik nygart <frederik nygart>
2017-07-13 12:03:58 +0200

    added new reducer based on action getCenters

--- src/js/reducers/center_reducer.js
+++ src/js/reducers/center_reducer.js
@@ -1,6 +1,10 @@
 import createReducer from '../lib/reducer_helper';
+import centers from '../data/mocks/centers';
 import * as types from '../actions/types';
 
-export const centers = createReducer({}, {
 
+export const allCenters = createReducer([], {
+    [types.GET_CENTERS](state, actions) {
+        return state.concat(centers);
+    }
 });
