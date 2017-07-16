bf9288af20617943e09ab0529ec7ea15fc004469
frederik nygart <frederik nygart>
2017-07-16 21:15:59 +0200

    Added a new goto type

--- src/js/actions/types.js
+++ src/js/actions/types.js
@@ -1,3 +1,6 @@
 export const SEARCH_CENTER = 'SEARCH_CENTER';
 export const SELECT_CENTER = 'SELECT_CENTER';
 export const GET_CENTERS = 'GET_CENTERS';
+
+//NAVIGATION
+export const GO_TO = 'GO_TO';
