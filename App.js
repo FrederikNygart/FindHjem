2bfe348ee366b98758b5b68b9c53a86497ef411d
frederik nygart <frederik nygart>
2017-07-17 15:32:26 +0200

    Added log to see store state
    Signed-off-by: frederik nygart <frederik nygart>

--- App.js
+++ App.js
@@ -24,6 +24,10 @@ function configureStore(initialState) {
 
 const store = configureStore({});
 
+store.subscribe(() => {
+  console.log('Store changed', store.getState());
+});
+
 export default class App extends Component {
   render() {
     return (
