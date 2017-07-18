d8439f33458b9ae1471236ab9decbf05682e8190
frederik nygart <frederik nygart>
2017-07-18 12:53:19 +0200

    cleaned up center reducer

--- src/js/reducers/center_reducer.js
+++ src/js/reducers/center_reducer.js
@@ -1,13 +1,12 @@
 import centers from '../data/mocks/centers';
 import * as types from '../actions/types';
 
-export default function centerReducer(state = [], action){
+export default function centerReducer(state = [], action) {
     switch (action.type) {
         case types.GET_CENTERS:
-            return {...state, centers: centers}
-    
+            return { ...state, centers: centers }
+
         default:
-        return state;
-            break;
+            return state;
     }
 }
