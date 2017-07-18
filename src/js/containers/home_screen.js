6565e72c6988b2ce25811760fc71ee5c7648ea55
frederik nygart <frederik nygart>
2017-07-18 12:52:31 +0200

    finished design of homescreen

--- src/js/containers/home_screen.js
+++ src/js/containers/home_screen.js
@@ -1,49 +1,119 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { goTo } from '../actions';
+import { goTo, setAge, setGender } from '../actions';
 import { appStyle } from '../styles';
-import { 
-  Text, 
-  View, 
-  Button 
+
+import {
+  Text,
+  TextInput,
+  Picker,
+  View,
+  Button
 } from 'react-native';
 
-import InfoBox from '../components/info_box';
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+  RadioButtonLabel
+} from 'react-native-simple-radio-button';
+
+const radioProps = [
+  { label: 'Mand', value: 1 },
+  { label: 'Kvinde', value: 2 },
+]
 
 class HomeScreen extends Component {
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: '',
+      gender: 1,
+      welcomeHeader: 'Velkommen til Find Hjem!',
+      welcomeSubText: 'Indtast din alder og dit køn for at finde hjælp.'
+    }
+  }
+
   static navigationOptions = {
-    title: 'Find hjem!'
+    title: 'Find Hjem!'
   };
-  
+
   render() {
     return (
       <View style={appStyle.container}>
-        <InfoBox
-          infoText={"this is some hardcoded info text"}
-        />
-        <Button
-          title='Get Centers'
-          onPress={
-            () => { 
-              this.props.goTo('CenterList'); 
+        <View style={appStyle.containerCentered}>
+          <View style={appStyle.containerCentered}>
+            <Text>{this.state.welcomeHeader}</Text>
+            <Text>{this.state.welcomeSubText}</Text>
+          </View>
+          <View style={appStyle.row}>
+            <View style={appStyle.containerBox}>
+              <Text>Alder:</Text>
+            </View>
+            <View style={appStyle.containerBox}>
+              <TextInput
+                style={appStyle.textInput}
+                keyboardType={'numeric'}
+                selectionColor={'steelblue'}
+                onChangeText={(text) => {
+                  this.setState({ age: text })
+                }
+                }
+                value={this.state.age}
+              ></TextInput>
+            </View>
+          </View>
+          <View style={appStyle.row}>
+            <View style={appStyle.containerBox}>
+              <Text>Køn:</Text>
+            </View>
+            <View style={appStyle.containerBox}>
+              <RadioForm
+                radio_props={radioProps}
+                initial={1}
+                animation={true}
+                onPress={(value) => {
+                  this.setState({ gender: value })
+                }
+                }
+              >
+              </RadioForm>
+            </View >
+          </View>
+        </View>
+        <View style={appStyle.containerCentered}>
+          <Button
+            title='Find hjælp!'
+            onPress={
+              () => {
+                this.props.goTo('CenterList');
+                this.props.setAge(this.state.age);
+                this.props.setGender(this.state.gender);
+              }
             }
-          }
-        />
-
+          />
+        </View>
       </View>
     );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
     {
-      goTo: goTo,
-    }, 
+      goTo,
+      setAge,
+      setGender,
+    },
     dispatch
   );
 }
 
-export default connect(null, mapDispatchToProps)(HomeScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
