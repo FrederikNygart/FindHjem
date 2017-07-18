be82258cf5c28eeebb0c72a48ad307740803952a
frederik nygart <frederik nygart>
2017-07-18 12:53:00 +0200

    added a user reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import centerReducer from './center_reducer';
 import navigationReducer from './navigation_reducer';
+import userReducer from './user_reducer';
 
 export default reducer = combineReducers({
     centers: centerReducer,
-    nav: navigationReducer
+    nav: navigationReducer,
+    user: userReducer,
 });
---
+++ src/js/reducers/user_reducer.js
@@ -0,0 +1,12 @@
+import * as types from '../actions/types';
+
+export default function userReducer(state = {}, action) {
+    switch (action.type) {
+        case types.SET_AGE:
+            return { ...state, age: action.age };
+        case types.SET_GENDER:
+            return { ...state, gender: action.gender };
+        default:
+            return state;
+    }
+}
