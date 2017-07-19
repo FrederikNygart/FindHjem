f71313e32051bb66d4c638bbee4818830cddebc1
frederik nygart <frederik nygart>
2017-07-19 22:53:55 +0200

    added center_classification

---
+++ src/js/data/mocks/center_classification.js
@@ -0,0 +1,13 @@
+const FIRST_LEVEL = {
+    ADDICTION: 'Misbrugsproblemer',
+    SOCIALIZING: 'Jeg mangler socialt samvær',
+    ADVICE: 'Jeg har brug for rådgivning',
+    ACTIVATION: 'Jeg mangler undervisning og/eller aktivering',
+    SHELTER: 'Jeg har ingen steder at bo eller sove',
+    EDUCATION_HELP: 'Jeg søger uddannelseshjælp',
+    ECONOMIC_HELP: 'Jeg søger kontanthjælp',
+    MEDICAL_HELP: 'Jeg mangler lægehjælp',
+    FOOD_AND_HYGIENE: 'Jeg er sulten eller ønsker et bad',
+}
+
+export {FIRST_LEVEL};
