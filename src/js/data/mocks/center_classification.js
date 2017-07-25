b6ce5e36b5555c3a52d85b4d3be51d7c2539680e
HappyFisk <frederiknygart@gmail.com>
2017-07-25 13:48:55 +0200

    Added centers array to open possibility of diversity in implementation.

--- src/js/data/mocks/center_classification.js
+++ src/js/data/mocks/center_classification.js
@@ -1,123 +1,168 @@
+import { CENTERS } from './centers';
+
 const CATAGORIES = {
     ADDICTION: {
         description: 'Misbrugsproblemer',
+        centers: [
+        ],
         subCatagories: {
             TREATMENT: {
                 description: 'Behandling',
+                centers: [
+
+                ],
             },
             ALCOHOL_PROBLEMS: {
-                description: 'Rådgivning til dig med alkoholproblemer'
+                description: 'Rådgivning til dig med alkoholproblemer',
+                centers: [],
             }
         }
     },
     SOCIALIZING: {
         description: 'Jeg mangler socialt samvær',
+        centers: [],
         subCatagories: {
             YOUTH: {
                 description: 'Psykisk sårbare unge',
+                centers: []
             },
             FORMER_FOSTERCARE: {
-                description: 'Tidligere anbragt'
+                description: 'Tidligere anbragt',
+                centers: []
             },
             SHELTER: {
                 description: 'Varmestue',
+                centers: []
             },
             GREENLANDIC: {
-                description: 'Grønlænder'
+                description: 'Grønlænder',
+                centers: []
             },
             EX_ADDICT: {
-                description: 'Eksmisbruger'
+                description: 'Eksmisbruger',
+                centers: [
+                ]
             },
         }
     },
     ADVICE: {
         description: 'Jeg har brug for rådgivning',
+        centers: [],
         subCatagories: {
             YOUTH: {
                 description: 'Rådgivning til unge',
+                centers: [],
             },
             PROSTITUTION: {
-                description: 'Jeg har været ude i prositution'
+                description: 'Jeg har været ude i prositution',
+                centers: [],
             },
             NIGHT_TIME: {
                 description: 'Akut hjælp i nattetimerne',
+                centers: [],
             },
             LEGAL: {
-                description: 'Juridisk hjælp'
+                description: 'Juridisk hjælp',
+                centers: [],
             },
             PREGNANCY_ABORTION: {
-                description: 'Gravid/abort problemer'
+                description: 'Gravid/abort problemer',
+                centers: [],
             },
             SEXUAL_ASSAULT: {
-                description: 'Udsat for seksuelle overgreb'
+                description: 'Udsat for seksuelle overgreb',
+                centers: [],
             }
         }
     },
     ACTIVATION_EDUCATION: {
         description: 'Jeg mangler undervisning og/eller aktivering',
+        centers: [],
         subCatagories: {
             MENTAL_PROBLEMS: {
                 description: 'Psykiske problemer',
+                centers: [],
             },
             GENERAL: {
-                description: 'Diverse aktiviteter'
+                description: 'Diverse aktiviteter',
+                centers: [],
             },
             FARMING: {
-                description: 'Gårdprojekt'
+                description: 'Gårdprojekt',
+                centers: [],
             },
             EDUCATION_HELP: {
-                description: 'Jeg søger uddannelseshjælp'
+                description: 'Jeg søger uddannelseshjælp',
+                centers: [],
             }
         }
     },
     ACCOMODATION: {
         description: 'Jeg har ingen steder at bo eller sove',
+        centers: [],
         subCatagories: {
             DOMESTIC_VIOLENCE: {
                 description: 'Udsat for vold',
+                centers: [],
             },
             EMERGENCY_ACCOMODATION_ALL: {
-                description: 'Akut overnatning dag + nat tilbud'
+                description: 'Akut overnatning dag + nat tilbud',
+                centers: [],
             },
             EMERGENCY_ACCOMODATION_NIGHT: {
-                description: 'Akut overnatning nat tilbud'
+                description: 'Akut overnatning nat tilbud',
+                centers: [],
             },
             EMERGENCY_ACCOMODATION_WOMEN: {
-                description: 'Akut overnatning for kvinder'
+                description: 'Akut overnatning for kvinder',
+                centers: [],
             },
             GUIDANCE: {
-                description: 'Akut overnatning dag + nat'
+                description: 'vejledning',
+                centers: [
+                ],
             },
         }
     },
     ECONOMIC_HELP: {
         description: 'Jeg søger kontanthjælp',
+        centers: [],
         subCatagories: {
             UNEMPLOYED: {
                 description: 'TRIN 1: Meld dig ledig',
+                centers: [],
             },
             UNEMPLOYMENT_BENEFITS: {
-                description: 'TRIN 2: Søg kontanthjælp '
+                description: 'TRIN 2: Søg kontanthjælp ',
+                centers: [],
             }
         }
     },
     MEDICAL_HELP: {
-        description: 'Jeg mangler lægehjælp'
+        description: 'Jeg mangler lægehjælp',
+        centers: [],
+        subCatagories: {
+        }
     },
     FOOD_AND_HYGIENE: {
         description: 'Jeg er sulten eller ønsker et bad',
+        centers: [],
         subCatagories: {
             FREE_BREAKFAST: {
                 description: 'Gratis morgenmad',
+                centers: [],
             },
             CHEAP_FOOD_AND_BATH: {
-                description: 'Billig mad, bad og tøj'
+                description: 'Billig mad, bad og tøj',
+                centers: [],
             },
             FREE_FOOD_AND_BATH: {
                 description: 'Gratis mad, bad og rent tøj',
+                centers: [],
             },
             LAUNDRY: {
-                description: 'Billig mad, gratis bad og tøjvask til 20 kr.'
+                description: 'Billig mad, gratis bad og tøjvask til 20 kr.',
+                centers: [],
             }
         }
     },
