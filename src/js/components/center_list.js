a27459e5d15c26dd30cb88e8fa624dc18e2e9415
HappyFisk <frederiknygart@gmail.com>
2017-08-13 16:51:58 +0200

    passed extra prop through to center list  and refactored props in center list screen

--- src/js/components/center_list.js
+++ src/js/components/center_list.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import { appStyle } from '../lib/styles';
 import {
     View,
-    Text
+    Text,
+    TouchableHighlight,
 } from 'react-native';
 
-const CenterListElement = ({ centerName }) => (
-    <View style={appStyle.listElement}>
-        <Text>
-            {centerName}
-        </Text>
-    </View>
+const goToCenter = (selectCenter, onClick, centerName) => {
+    selectCenter(centerName);
+    onClick('Center', { centerName: centerName });
+}
+
+const CenterListElement = ({ selectCenter, onClick, centerName, }) => (
+    <TouchableHighlight
+        onPress={() => goToCenter(selectCenter, onClick, centerName)}
+        activeOpacity={0.99}
+        underlayColor={'steelblue'}
+    >
+        <View style={appStyle.listElement}>
+            <Text>
+                {centerName}
+            </Text>
+        </View>
+    </TouchableHighlight>
 );
 
-export const CenterList = ({ centers }) => (
+export const CenterList = ({ selectCenter, onClick, centerNames, }) => (
     <View>
         {
-            centers.map((center, i) => {
+            centerNames.map((centerName, i) => {
                 return (
                     <CenterListElement
                         key={i}
-                        centerName={center.name}
+                        selectCenter={selectCenter}
+                        onClick={onClick}
+                        centerName={centerName}
                     />
                 )
             })
--- src/js/containers/center_list_screen.js
+++ src/js/containers/center_list_screen.js
@@ -2,35 +2,49 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { goTo } from '../actions';
+import { HeaderBackButton } from 'react-navigation';
 import { appStyle } from '../lib/styles';
-import {CenterList} from '../components/center_list'
+import * as types from '../lib/types';
+import { CenterList } from '../components/center_list'
+import {
+  goTo,
+  selectCenter
+} from '../actions';
 
 class CenterListScreen extends Component {
-  
-  static navigationOptions = {
-    title: 'Centres'
-  };
-  
+
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Centre',
+    headerLeft: <HeaderBackButton onPress={() => navigation.dispatch({ type: types.POP_CATAGORY })} />,
+  });
+
   render() {
     return (
       <ScrollView style={appStyle.container}>
         <CenterList
-          centers={this.props.centers}
-        /> 
+          selectCenter={this.props.selectCenter}
+          onClick={this.props.goTo}
+          centerNames={this.props.centers}
+        />
       </ScrollView>
     );
   }
 }
 
-function mapStateToProps(state){
-  return{
-    centers: state.centers.current,
+function mapStateToProps(state) {
+  return {
+    centers: state.catagories.centers,
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({goTo: goTo}, dispatch);
+  return bindActionCreators(
+    {
+      goTo,
+      selectCenter
+    },
+    dispatch
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CenterListScreen);
