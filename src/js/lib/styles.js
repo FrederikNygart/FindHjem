a8827eb19e7aa1e781b35df2cabedcf3d3d55900
HappyFisk <frederiknygart@gmail.com>
2017-08-13 16:52:23 +0200

    refactored styles

--- src/js/lib/styles.js
+++ src/js/lib/styles.js
@@ -1,5 +1,74 @@
 import { StyleSheet } from 'react-native';
 
+export const style = {
+  openingHours: StyleSheet.create({
+    container: {
+      margin: 20,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: 'skyblue',
+      borderRadius: 10,
+    },
+    element: {
+      marginTop: 5,
+      marginBottom: 5,
+      marginLeft: 10,
+      marginRight: 30,
+    },
+    textDay: {
+      textAlign: 'left'
+    },
+    dayContainer: {
+      width: 100,
+      marginLeft: 40,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start'
+    },
+    textHours: {
+      marginLeft: 10,
+      textAlign: 'center'
+    },
+    hoursContainer: {
+      width: 100,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start'
+    },
+    row: {
+      flex: 1,
+      flexDirection: 'row',
+    }
+  }),
+  centerInfo: StyleSheet.create({
+    container: {
+      flex: -1,
+      marginTop: 20,
+      marginBottom: 10
+    },
+    row: {
+      flex: -1,
+      flexDirection: 'row',
+    },
+    element: {
+      marginTop: 5,
+      marginBottom: 5,
+      marginLeft: 30,
+    },
+    text: {
+      textAlign: 'left',
+      marginLeft: 10,
+    },
+    textContainer: {
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start'
+    },
+    iconContainer: {
+      width: 10,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+  }),
+}
+
 export const appStyle = StyleSheet.create({
   resultText: {
     flex: 1,
@@ -9,19 +78,6 @@ export const appStyle = StyleSheet.create({
   centerDescription: {
     flex: 1,
     textAlign: 'left',
-    marginLeft: 30,
-    marginRight: 30,
-  },
-  openingHours:{
-    margin: 20,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'skyblue',
-    borderRadius: 10,
-  },
-  openingHoursElement: {
-    marginTop: 10,
-    marginBottom: 10,
     marginLeft: 30,
     marginRight: 30,
   },
@@ -88,3 +144,5 @@ export const appStyle = StyleSheet.create({
     height: 100,
   },
 });
+
+
